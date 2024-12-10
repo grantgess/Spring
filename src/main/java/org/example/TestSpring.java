efcff9cd355cfa7839b3df085bdfebb6b86a33ce
grantgess <hhh.kqw146@gmail.com>
2024-12-10 23:01:23 +0300

    внедрение зависимоствей создание бина musicPlayer из конфиг файла

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,8 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music= context.getBean("musicBean", Music.class);
-        MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
         player.playMusic();
         context.close();
     }

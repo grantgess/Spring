abca96af9fb2bba5d21eda14ea3f64c6ab1d4f8e
grantgess <hhh.kqw146@gmail.com>
2024-12-12 18:26:39 +0300

    аннотация component

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component("musicBean")
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,13 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
-        player1.playMusic();
-        player1.setVolume(10);
-
-        System.out.println(player1.getVolume());
-        System.out.println(player2.getVolume());
+        Music music = context.getBean("music", ClassicalMusic.class);
+        System.out.println(music.getSong());
         context.close();
     }
 

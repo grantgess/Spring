b564447f8b3c1b5b8bd35deb6ed65ed1b0814945
grantgess <hhh.kqw146@gmail.com>
2024-12-12 18:07:56 +0300

    scope prototype

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -5,10 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
-        player.playMusic();
-        System.out.println(player.getName());
-        System.out.println(player.getVolume());
+        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
+        player1.playMusic();
+        player1.setVolume(10);
+
+        System.out.println(player1.getVolume());
+        System.out.println(player2.getVolume());
         context.close();
     }
 
